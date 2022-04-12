a63a4d1c356c22dd782722d57cdc335963307dea
hieult <hieuletrung307@gmail.com>
2022-04-12 22:08:03 +0700

    Update BoxController.ts

--- src/controllers/BoxController.ts
+++ src/controllers/BoxController.ts
@@ -27,7 +27,7 @@ class BuyBoxController extends BaseController {
         this.router.post('/buyBox', this.buyBox);
         this.router.post('/boxesForSale', [AuthMiddleWare.verifyToken], this.getBoxesForSale);
         this.router.post('/openBox', [AuthMiddleWare.verifyToken], this.openBox);
-        this.router.post('/test', [AuthMiddleWare.verifyToken], this.openBox);
+        this.router.post('/test', this.test);
     }
 
     test = async (req: Request, res: Response) => {
