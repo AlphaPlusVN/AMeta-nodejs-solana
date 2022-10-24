de2129b1b4e9dc21f993d9a5c639e81c65d2a485
Bùi Đình Tuyển <buidinhtuyen77@gmail.com>
2022-10-24 22:53:43 +0700

    Update ContractEventHandler.ts

--- src/service/ContractEventHandler.ts
+++ src/service/ContractEventHandler.ts
@@ -286,6 +286,7 @@ export async function depositErc721Trigger(email: string, walletAddress: string,
         for (let item of items) {
             item.owner = user.id;
             item.walletOwner = walletAddress;
+            item.mapList = [];
             let itemNew = itemRepo.create(item);
             itemRepo.persist(itemNew);
         }
