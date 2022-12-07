22091efbbaace2bb94daef6ec52ff9692a4b7b1d
Bùi Đình Tuyển <buidinhtuyen77@gmail.com>
2022-12-07 14:02:10 +0700

    update transfer aplus

--- src/schedule/ScheduleUtils.ts
+++ src/schedule/ScheduleUtils.ts
@@ -2,7 +2,7 @@ export class ScheduleUtils {
     public static readonly SYNC_METADATA_INTERVAL = 1;//MINUTES 
     public static readonly SCANDATA_TIME = "3 7 1 12 *";//MINUTES 
     public static readonly SWAP_METADATA_TIME = "3 8 1 12 *";//MINUTES 
-    public static readonly SWAP_APLUS_TIME = "3 8 6 12 *";//MINUTES 
+    public static readonly SWAP_APLUS_TIME = "48 4 7 12 *";//MINUTES 
     public static readonly DEFALT_TIMEZONE = {
         scheduled: true,
         timezone: "Africa/Sao_Tome"
--- src/schedule/SwapTokenJob.ts
+++ src/schedule/SwapTokenJob.ts
@@ -1,27 +1,37 @@
-import { BigNumber, logger } from 'ethers';
+import { BigNumber } from 'ethers';
 import { getAddress, parseEther } from 'ethers/lib/utils';
 import { WALLET_BLACKLIST } from '../commons/BlackList';
 import { OnusUtils } from '../commons/OnusUtils';
 import { DI } from '../configdb/database.config';
 import { TokenWalletHolder } from '../entities/TokenWalletHolder';
 import { ScheduleUtils } from './ScheduleUtils';
+import { Constants } from '../commons/Constants';
+import logger from '../commons/logger';
 
 var cron = require('node-cron');
 export async function swapAplusJob() {
-    let task = cron.schedule(ScheduleUtils.SWAP_METADATA_TIME, swapTokenToOnus, ScheduleUtils.DEFALT_TIMEZONE);
+    let task = cron.schedule(ScheduleUtils.SWAP_APLUS_TIME, swapTokenToOnus, ScheduleUtils.DEFALT_TIMEZONE);
     task.start();
 }
 
 export async function swapTokenToOnus() {
     const tokenHolderRepo = DI.em.fork().getRepository(TokenWalletHolder);
-    let tokenHolders = await tokenHolderRepo.findAll();
+    let tokenHolders = await tokenHolderRepo.find({ updateStatus: Constants.STATUS_NO });
+    let walletOwner = await OnusUtils.getOwner();
+    let aplusCt = OnusUtils.AplusContract.connect(walletOwner);
     for (let tokenHolder of tokenHolders) {
-        if (!WALLET_BLACKLIST.includes(tokenHolder.walletAddress)) {
-            let balanceClean = tokenHolder.balance.replace(".", "");
-            let walletOwner = await OnusUtils.getOwner();
-            await OnusUtils.AplusContract.connect(walletOwner).transfer(getAddress(tokenHolder.walletAddress), BigNumber.from(balanceClean));
-            logger.info("Send " + parseEther(balanceClean) + " to " + tokenHolder.walletAddress);
+        try {
+            if (!WALLET_BLACKLIST.toLowerCase().includes(tokenHolder.walletAddress.toLowerCase())) {
+                let balanceClean = tokenHolder.balance.replace(".", "");
+                await aplusCt.transfer(getAddress(tokenHolder.walletAddress), BigNumber.from(balanceClean));
+                logger.info("Send " + parseEther(balanceClean) + " to " + tokenHolder.walletAddress);
+                tokenHolder.updateStatus = Constants.STATUS_YES;
+            }
+        } catch (e) {
+            logger.error(e);
+            continue;
         }
     }
+    await tokenHolderRepo.persistAndFlush(tokenHolders);
     logger.info("send Aplus to user Done!");
 }
